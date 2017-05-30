e5bb55ffd983b36749d7f0c5e9d9ca051b12ac27
jonkofee <jonkofee@icloud.com>
2017-05-30 19:04:02 +0300

    Роут для деплоя

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require("express");
-var app     = express();
-var path    = require("path");
-var port    = 8080;
+var app = express();
+var path = require("path");
+var exec = require('child_process').exec;
+var port = 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.get('/deploy', function(req, res) {
+  exec('git pull', function(error, stdout, stderr) {
+    if (!error) {
+      exec('npm run build', function(error, stdout, stderr) {
+        error ? res.send(error) : res.send("OK");
+      });
+    }
+  });
 });
 
 app.listen(port);
 
 console.log("Running at Port " + port);
-
