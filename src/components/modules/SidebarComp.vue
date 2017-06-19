ae80fe573bb1adb7c1f78074b9e3f8f9af3ab3e6
Andrey Klimash <andrey.klimash@gmail.com>
2017-06-19 07:50:52 +0300

    Добавил навигацию

--- src/components/modules/SidebarComp.vue
+++ src/components/modules/SidebarComp.vue
@@ -1,13 +1,92 @@
 <template>
 
     <div class="sidebar">
-        <slot></slot>
+        <div class="sidebar_child">
+            <button-comp
+                text="Создать накладную"
+                :disabled="disabledButton"
+                @click.native="$emit('openFormInvoice')">
+            </button-comp>
+        </div>
+        <div class="sidebar_child" v-for="item in menu">
+            <sidebar-category
+                :title="item.title"
+                :menuItems="item.menuItems">
+            </sidebar-category>
+        </div>
     </div>
 
 </template>
 
 <script>
+import ButtonComp from '../UI/ButtonComp.vue';
+import SidebarCategory from './SidebarCategory.vue';
+
 export default {
+    props: {
+        disabledButton: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            menu: [
+                {
+                    title: 'Накладные',
+                    menuItems: [
+                        {
+                            name: 'К отправлению',
+                            value: 7
+                        },
+                        {
+                            name: 'К получению',
+                            value: 12
+                        },
+                        {
+                            name: 'К выдаче',
+                            value: 5
+                        }
+                    ]
+                },
+                {
+                    title: 'Ведомости',
+                    menuItems: [
+                        {
+                            name: 'Погрузочные',
+                            value: 7
+                        },
+                        {
+                            name: 'Выгрузочные',
+                            value: 12
+                        },
+                        {
+                            name: 'Касса',
+                            value: '3750 р'
+                        }
+                    ]
+                },
+                {
+                    title: 'Справка',
+                    menuItems: [
+                        {
+                            name: 'Документы для работы'
+                        },
+                        {
+                            name: 'Как работать в системе'
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    methods: {
+
+    },
+    components: {
+        ButtonComp,
+        SidebarCategory
+    }
 }
 </script>
 
@@ -15,16 +94,19 @@ export default {
 
     @import "../../configs/styles_config.sass"
 
-    *
-        box-sizing: border-box
-
     .sidebar
         position: fixed
-        width: 300px
+        width: 260px
         height: 900px
         top: 60px
         left: 0
         background: #fff
         z-index: 0
+
+        &_child
+            padding: 15px 20px
+
+            & + &
+                border-top: 1px solid $medium
 
 </style>
